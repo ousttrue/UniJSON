235bbf111f77ac28d38cdec42c988c1540008474
ousttrue <ousttrue@gmail.com>
2018-08-09 15:35:06 +0900

    Added ParseTest

--- Editor/Tests/ParserTests.cs
+++ Editor/Tests/ParserTests.cs
@@ -1,5 +1,7 @@
 ﻿using NUnit.Framework;
-
+using System.IO;
+using System.Text;
+using UnityEngine;
 
 namespace UniJSON
 {
@@ -16,6 +18,17 @@ namespace UniJSON
             {
                 var result = JsonParser.Parse("{ \"a\": { \"b\": 1 }}");
                 Assert.True(result.ContainsKey("a"));
+            }
+        }
+
+        [Test]
+        public void PathGithub()
+        {
+            var path = Path.GetFullPath(Application.dataPath + "/../tmp.json");
+            if(File.Exists(path))
+            {
+                var json = File.ReadAllText(path, Encoding.UTF8);
+                var result = JsonParser.Parse(json);
             }
         }
     }
