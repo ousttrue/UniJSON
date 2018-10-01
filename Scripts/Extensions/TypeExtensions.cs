5055f6a2c37b8d66eeb8797c5c70a01917e2de2d
ousttrue <ousttrue@gmail.com>
2018-10-01 21:37:15 +0900

    Added JsonDictionaryValidator

--- Scripts/Extensions/TypeExtensions.cs
+++ Scripts/Extensions/TypeExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 
@@ -8,7 +9,16 @@ namespace UniJSON
     {
         public static bool GetIsGenericList(this Type t)
         {
-            return t.IsGenericType && (t.GetGenericTypeDefinition() == typeof(List<>));
+            return t.IsGenericType 
+                && (t.GetGenericTypeDefinition() == typeof(List<>));
+        }
+
+        public static bool GetIsGenericDictionary(this Type t)
+        {
+            return t.IsGenericType 
+                && (t.GetGenericTypeDefinition() == typeof(Dictionary<,>)
+                && t.GetGenericArguments().FirstOrDefault()==typeof(string)
+                );
         }
     }
 }
