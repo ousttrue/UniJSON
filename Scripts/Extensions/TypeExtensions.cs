bcb82ce204d107b473184ead1260ded5221d9732
ousttrue <ousttrue@gmail.com>
2018-10-03 21:07:40 +0900

    Added null check to GetIsGenericDictionary

--- Scripts/Extensions/TypeExtensions.cs
+++ Scripts/Extensions/TypeExtensions.cs
@@ -9,15 +9,19 @@ namespace UniJSON
     {
         public static bool GetIsGenericList(this Type t)
         {
-            return t.IsGenericType 
+            if (t == null) return false;
+
+            return t.IsGenericType
                 && (t.GetGenericTypeDefinition() == typeof(List<>));
         }
 
         public static bool GetIsGenericDictionary(this Type t)
         {
-            return t.IsGenericType 
+            if (t == null) return false;
+
+            return t.IsGenericType
                 && (t.GetGenericTypeDefinition() == typeof(Dictionary<,>)
-                && t.GetGenericArguments().FirstOrDefault()==typeof(string)
+                && t.GetGenericArguments().FirstOrDefault() == typeof(string)
                 );
         }
     }
