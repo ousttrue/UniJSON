ca76c691ee7833fdd46cc19f0afea69935958e6f
ousttrue <ousttrue@gmail.com>
2018-11-28 16:20:11 +0900

    Added KeyValueTest

--- Editor/Tests/Json/JsonSerializerTests.cs
+++ Editor/Tests/Json/JsonSerializerTests.cs
@@ -20,6 +20,8 @@ namespace UniJSON
         {
             public float X;
             public float Y;
+
+            public float[] Vector;
         }
 
         [Test]
@@ -41,12 +43,31 @@ namespace UniJSON
             //SerializeValue(new object[] { null, 1, "a" }, "[null,1,\"a\"]");
 
             SerializeValue(new Dictionary<string, object> { }, "{}");
-            SerializeValue(new Dictionary<string, object> { {"a", 1} }, "{\"a\":1}");
+            SerializeValue(new Dictionary<string, object> { { "a", 1 } }, "{\"a\":1}");
             SerializeValue(new Dictionary<string, object> { { "a",
                     new Dictionary<string, object>{
                     } } }, "{\"a\":{}}");
 
             SerializeValue(new Point(), "{\"X\":0,\"Y\":0}");
+        }
+
+        [Test]
+        public void KeyValue()
+        {
+            var p = new Point
+            {
+                X = 1,
+                Vector = new float[] { 1, 2, 3 }
+            };
+
+            var f = new JsonFormatter();
+            f.BeginMap();
+            f.KeyValue(() => p.Vector);
+            f.EndMap();
+
+            var json = JsonParser.Parse(new Utf8String(f.GetStore().Bytes));
+
+            Assert.AreEqual(1, json["Vector"][0].GetInt32());
         }
     }
 }
