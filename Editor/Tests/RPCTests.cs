157f454064eee92745fbd5ebdd68e6ffbd001d72
ousttrue <ousttrue@gmail.com>
2018-11-30 12:42:34 +0900

    Separated RpcDispatcher

--- Editor/Tests/RPCTests.cs
+++ Editor/Tests/RPCTests.cs
@@ -7,66 +7,6 @@ namespace UniJSON
 {
     public class RPCTests
     {
-        class RpcDispatcher
-        {
-            delegate void Callback(IValueNode args, IFormatter f);
-            Dictionary<string, Callback> m_map = new Dictionary<string, Callback>();
-
-            public void Register<A0>(string method, Action<A0> action)
-            {
-                throw new NotImplementedException();
-            }
-
-            public void Register<A0, A1>(string method, Action<A0, A1> action)
-            {
-                throw new NotImplementedException();
-            }
-
-            public void Register<A0, A1, R>(string method, Func<A0, A1, R> action)
-            {
-                m_map.Add(method, (args, f) =>
-                {
-                    var it = args.ArrayItems.GetEnumerator();
-
-                    var a0 = default(A0);
-                    it.MoveNext();
-                    it.Current.Deserialize(ref a0);
-
-                    var a1 = default(A1);
-                    it.MoveNext();
-                    it.Current.Deserialize(ref a1);
-
-                    var r = action(a0, a1);
-                    f.Serialize(r);
-                });
-            }
-
-            public void Call(string method, IValueNode args, IFormatter f = null)
-            {
-                Callback callback;
-                if (!m_map.TryGetValue(method, out callback))
-                {
-                    throw new KeyNotFoundException();
-                }
-                callback(args, f);
-            }
-        }
-
-        static void Print(string msg)
-        {
-
-        }
-
-        static int Add(int a, int b)
-        {
-            return a + b;
-        }
-
-        static string Join(string a, string b, string c)
-        {
-            return a + "," + b + "," + c;
-        }
-
         [Test]
         public void RequestTest()
         {
@@ -99,16 +39,30 @@ namespace UniJSON
         public void DispatcherTest()
         {
             var dispatcher = new RpcDispatcher();
-            dispatcher.Register("add", (int a, int b) => a + b);
-
             var rpc = new JsonRpc();
-            var request = rpc.Request("add", 1, 2);
 
-            var f = new JsonFormatter();
-            var parsed = JsonParser.Parse(new Utf8String(request));
-            dispatcher.Call(parsed["method"].GetString(), parsed["params"], f);
+            {
+                dispatcher.Register("add", (int a, int b) => a + b);
+                var request = rpc.Request("add", 1, 2);
 
-            Assert.AreEqual(Utf8String.From("3"), new Utf8String(f.GetStore().Bytes));
+                var parsed = JsonParser.Parse(new Utf8String(request));
+                var f = new JsonFormatter();
+                dispatcher.Call(parsed["method"].GetString(), parsed["params"], f);
+
+                Assert.AreEqual(Utf8String.From("3"), new Utf8String(f.GetStore().Bytes));
+            }
+
+            {
+                string msg = null;
+                dispatcher.Register("print", (string _msg) => { msg = _msg; });
+                var request = rpc.Request("print", "hoge");
+
+                var parsed = JsonParser.Parse(new Utf8String(request));
+                var f = new JsonFormatter();
+                dispatcher.Call(parsed["method"].GetString(), parsed["params"], f);
+
+                Assert.AreEqual("hoge", msg);
+            }
         }
     }
 }
---
+++ Scripts/RpcDispatcher.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace UniJSON
+{
+    public class RpcDispatcher
+    {
+        delegate void Callback(IValueNode args, IFormatter f);
+        Dictionary<string, Callback> m_map = new Dictionary<string, Callback>();
+
+        #region Action
+        public void Register<A0>(string method, Action<A0> action)
+        {
+            m_map.Add(method, (args, f) =>
+            {
+                var it = args.ArrayItems.GetEnumerator();
+
+                var a0 = default(A0);
+                it.MoveNext();
+                it.Current.Deserialize(ref a0);
+
+                action(a0);
+            });
+        }
+
+        public void Register<A0, A1>(string method, Action<A0, A1> action)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Func
+        public void Register<A0, A1, R>(string method, Func<A0, A1, R> action)
+        {
+            m_map.Add(method, (args, f) =>
+            {
+                var it = args.ArrayItems.GetEnumerator();
+
+                var a0 = default(A0);
+                it.MoveNext();
+                it.Current.Deserialize(ref a0);
+
+                var a1 = default(A1);
+                it.MoveNext();
+                it.Current.Deserialize(ref a1);
+
+                var r = action(a0, a1);
+                f.Serialize(r);
+            });
+        }
+        #endregion
+
+        public void Call(string method, IValueNode args, IFormatter f = null)
+        {
+            Callback callback;
+            if (!m_map.TryGetValue(method, out callback))
+            {
+                throw new KeyNotFoundException();
+            }
+            callback(args, f);
+        }
+    }
+}
