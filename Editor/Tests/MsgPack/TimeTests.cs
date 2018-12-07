9482dfa79629705c8cd81a9013dcd24e12e33410
ousttrue <ousttrue@gmail.com>
2018-12-07 13:25:47 +0900

    MsgPackFormatter.Time32

---
+++ Editor/Tests/MsgPack/TimeTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+
+
+namespace UniJSON
+{
+    public class TimeTests
+    {
+        [Test]
+        public void Time32Test()
+        {
+            var time = new DateTimeOffset(1970, 1, 1, 0, 0, 0, 0, TimeSpan.Zero);
+
+            var f = new MsgPackFormatter();
+            f.Time32(time);
+
+            var bytes = f.GetStoreBytes().ArrayOrCopy();
+            unchecked
+            {
+                Assert.AreEqual(new byte[]
+                {
+                0xd6, (byte)-1, 0, 0, 0, 0
+                }, bytes);
+            }
+        }
+    }
+}
---
+++ Scripts/Extensions/TimeExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace UniJSON
+{
+    public static class TimeExtensions
+    {
+#if !NET_4_6 && !NET_STANDARD_2_0
+        const long NSPerSecond = 100000000;
+        const double TicksToSecodns = 1.0 / NSPerSecond;
+        public readonly static DateTimeOffset EpocTime = new DateTimeOffset(1970, 1, 1, 0, 0, 0, 0, TimeSpan.Zero);
+        public static long ToUnixTimeSeconds(this DateTimeOffset now)
+        {
+            return (now - EpocTime).Ticks / NSPerSecond;
+        }
+#endif
+    }
+}
